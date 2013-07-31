3562e74ef29010312e2dc9603b74d05aae667dd7
dansan <daniel@admin-box.com>
2013-07-31 21:53:19 +0200

    allow POST from external for OpenID when not in DEBUG mode

--- sas/settings.py
+++ sas/settings.py
@@ -20,7 +20,7 @@ LOG_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
